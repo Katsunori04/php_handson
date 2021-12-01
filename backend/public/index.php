931d5d1dc771f627a5878b1dfe61adb3a098c000
Katsunori Fukui <katsunori.fukui04@gmail.com>
2021-12-01 23:58:06 +0900

    クラスとオブジェクト

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,14 +1,30 @@
 <html>
 
   <body>
-    This is test.
 
     <?php
-    
-    $a="Hello";
-    $b="world";
-    
-    echo "$a, $b";
+      class Human {
+        private $name;
+        private $birthday;
+        private $gender;
+
+        public function __construct($name, $birthday, $gender){
+          $this->name = $name;
+          $this->birthdat = $birthday;
+          $this->gender = $gender;
+        }
+
+        public function walk(){
+          echo "{$this->name}は歩きます<br/>";
+        }
+
+        public function eat(){
+          echo '食べます<br/>';
+        }
+      }
+      $human = new Human('なまええ', '1995/4/28', '男');
+      $human->eat();
+      $human->walk();
     ?>
 
   </body>
